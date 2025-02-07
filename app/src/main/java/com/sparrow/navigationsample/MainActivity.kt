6efbcf2f2ca97ae8fbb781cb1f3f9ac722b38d73
mirmohsin0001 <mirmohsin0001@gmail.com>
2025-02-08 00:46:48 +0530

    initial setup

---
+++ app/src/main/java/com/sparrow/navigationsample/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.sparrow.navigationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sparrow.navigationsample.ui.theme.NavigationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationSampleTheme {
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    FirstScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FirstScreen(){
+    Text("FirstScreen")
+}
