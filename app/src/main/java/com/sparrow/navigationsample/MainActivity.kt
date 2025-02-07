d91c4a5c3009ee8da2ce80b943c02b21073dffe5
mirmohsin0001 <mirmohsin0001@gmail.com>
2025-02-08 01:02:22 +0530

    setting up the first screen

--- app/src/main/java/com/sparrow/navigationsample/MainActivity.kt
+++ app/src/main/java/com/sparrow/navigationsample/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sparrow.navigationsample.ui.theme.NavigationSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +31,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavigationSampleTheme {
-                Surface (
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ){
+                ) {
                     FirstScreen()
                 }
             }
@@ -32,6 +43,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun FirstScreen(){
-    Text("FirstScreen")
+fun FirstScreen() {
+    val name = remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("This is the first screen", fontSize = 24.sp)
+        Spacer(Modifier.height(16.dp))
+        OutlinedTextField(
+            value = name.value, onValueChange = {
+                name.value = it
+            }
+        )
+        Spacer(Modifier.height(16.dp))
+        Button(onClick = {}) {
+            Text("Next Screen")
+        }
+    }
 }
